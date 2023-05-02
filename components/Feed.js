32cc81b398022436e2031c92cdf9c08b63c4347f
Dennis Asplind <skene@hotmail.com>
2023-05-02 19:25:17 +0200

    Feed responsive

--- components/Feed.js
+++ components/Feed.js
@@ -3,15 +3,16 @@ import Stories from './Stories';
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+      <section className='md:col-span-2'>
         {/* Stories */}
         <Stories />
         {/* Posts */}
         <Post />
       </section>
-      <section>
+      <section className='hidden md:inline-grid col-span-1'>
         {/* Mini Profile */}
+
         {/* Suggestions */}
       </section>
     </main>
