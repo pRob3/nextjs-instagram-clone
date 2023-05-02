a58fe5f4b570a5f094a450238747e434b33c5863
Dennis Asplind <skene@hotmail.com>
2023-05-02 18:02:58 +0200

    header and image

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
 
 export default function Post({ id, img, userImg, username, caption }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className='bg-white my-7 border rounded-md'>
+      {/* Post Header */}
+      <div className='flex items-center p-5'>
+        <img
+          className='h-12 rounded-full object-cover border p-1 mr-3'
+          src={userImg}
+          alt={username}
+        />
+        <p className='font-bold flex-1'>{username}</p>
+        <EllipsisHorizontalIcon className='h-5' />
+      </div>
+      {/* Post image */}
+      <img className='object-cover w-full' src={img} alt='' />
     </div>
   );
 }
