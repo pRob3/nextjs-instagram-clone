473f0bc9b33b80873aea60f9a3f4c41e872b37e3
Dennis Asplind <skene@hotmail.com>
2023-05-02 16:37:33 +0200

    sticky header

--- components/Header.js
+++ components/Header.js
@@ -5,44 +5,46 @@ import { HomeIcon } from '@heroicons/react/24/solid';
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
-      {/* Left */}
-      <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
-        <Image
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png'
-          fill
-          className='object-contain'
-        />
-      </div>
-      <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
-        <Image
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png'
-          fill
-          className='object-contain'
-        />
-      </div>
-
-      {/* Middle */}
-      <div className='relative mt-1'>
-        <div className='absolute top-2 left-2'>
-          <MagnifyingGlassIcon className='h-5 w-5 text-gray-500' />
+    <div className='shadow-sm border-b sticky top-0 bg-white z-30'>
+      <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+        {/* Left */}
+        <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
+          <Image
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png'
+            fill
+            className='object-contain'
+          />
         </div>
-        <input
-          type='text'
-          placeholder='Search...'
-          className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'
-        />
-      </div>
+        <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
+          <Image
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png'
+            fill
+            className='object-contain'
+          />
+        </div>
 
-      {/* Right */}
-      <div className='flex space-x-4 items-center'>
-        <HomeIcon className='hidden md:inline-flex h-6 w-6 cursor-pointer hover:scale-125 transition.transform duration-200 ease-out' />
-        <PlusCircleIcon className='h-6 w-6 cursor-pointer hover:scale-125 transition.transform duration-200 ease-out' />
-        <img
-          src='https://media.licdn.com/dms/image/C5603AQGdK493X-2G0Q/profile-displayphoto-shrink_800_800/0/1629999642500?e=2147483647&v=beta&t=Xm3UfIawdbGeWSGt-ngqmKWOLrTufjlSFSO2aIgVUlk'
-          alt='user-image'
-          className='h-10 w-10 rounded-full cursor-pointer hover:scale-125 transition.transform duration-200 ease-out'
-        />
+        {/* Middle */}
+        <div className='relative mt-1'>
+          <div className='absolute top-2 left-2'>
+            <MagnifyingGlassIcon className='h-5 w-5 text-gray-500' />
+          </div>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'
+          />
+        </div>
+
+        {/* Right */}
+        <div className='flex space-x-4 items-center'>
+          <HomeIcon className='hidden md:inline-flex h-6 w-6 cursor-pointer hover:scale-125 transition.transform duration-200 ease-out' />
+          <PlusCircleIcon className='h-6 w-6 cursor-pointer hover:scale-125 transition.transform duration-200 ease-out' />
+          <img
+            src='https://media.licdn.com/dms/image/C5603AQGdK493X-2G0Q/profile-displayphoto-shrink_800_800/0/1629999642500?e=2147483647&v=beta&t=Xm3UfIawdbGeWSGt-ngqmKWOLrTufjlSFSO2aIgVUlk'
+            alt='user-image'
+            className='h-10 w-10 rounded-full cursor-pointer hover:scale-125 transition.transform duration-200 ease-out'
+          />
+        </div>
       </div>
     </div>
   );
