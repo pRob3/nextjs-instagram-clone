4ab5a8a2b87d22a4934010f5713177033fbc26ca
Dennis Asplind <skene@hotmail.com>
2023-05-02 18:45:17 +0200

    post buttons

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+import {
+  EllipsisHorizontalIcon,
+  HeartIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  BookmarkIcon,
+} from '@heroicons/react/24/outline';
 
 export default function Post({ id, img, userImg, username, caption }) {
   return (
@@ -14,8 +19,18 @@ export default function Post({ id, img, userImg, username, caption }) {
         <p className='font-bold flex-1'>{username}</p>
         <EllipsisHorizontalIcon className='h-5' />
       </div>
-      {/* Post image */}
+
+      {/* Post Image */}
       <img className='object-cover w-full' src={img} alt='' />
+
+      {/* Post Buttons */}
+      <div className='flex justify-between px-4 pt-4'>
+        <div className='flex space-x-4'>
+          <HeartIcon className='btn' />
+          <ChatBubbleOvalLeftEllipsisIcon className='btn' />
+        </div>
+        <BookmarkIcon className='btn' />
+      </div>
     </div>
   );
 }
